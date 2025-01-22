c813f97942e9749f728c595f700d3406a01dcb05
Destiny Serna <destinyeserna@gmail.com>
2025-01-21 20:46:23 -0800

    Added Game and attributes

--- seeker.py
+++ seeker.py
@@ -1,3 +1,38 @@
+class Game:
+    playing = False
+    game_board = [[]]
+    user_board = [[]]
+    def __init__(self, score=0):
+        self.score = score
+    
+    def start_game(self):
+        self.game_board = [[" ",1,2,3,4,5],
+                           [1," "," "," "," "," "],
+                           [2," "," "," "," "," "],
+                           [3," "," "," "," "," "],
+                           [4," "," "," "," "," "],
+                           [5," "," "," "," "," "]]
+        self.user_board = [[" ",1,2,3,4,5],
+                           [1," "," "," "," "," "],
+                           [2," "," "," "," "," "],
+                           [3," "," "," "," "," "],
+                           [4," "," "," "," "," "],
+                           [5," "," "," "," "," "]]
+   
+    def print_board_square(self, char):
+        if isinstance(char, int):
+            print("  "+str(char)+"  ")
+        else:
+            print("["+char+"]")
+
+    def print_user_board(self):
+        for row in self.user_board:
+            for col in row:
+                self.print_board_square(col)
+
+game = Game()
+game.start_game()
+
 print("")
 print("LETS PLAY SEEKER! :D")
 print("")
@@ -5,12 +40,7 @@ start = input("PRESS ANY KEY TO START")
 print("")
 print("HERE IS THE BOARD")
 print("")
-print("  1  2  3  4  5 ")
-print("1[ ][ ][ ][ ][ ]")
-print("2[ ][ ][ ][ ][ ]")
-print("3[ ][ ][ ][ ][ ]")
-print("4[ ][ ][ ][ ][ ]")
-print("5[ ][ ][ ][ ][ ]")
+game.print_user_board()
 print("")
 selected_row = input("SELECT A ROW: ")
 print("")
