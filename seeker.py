e6ca016de2a6aff8b0f736d5b146b7c8578fe6e1
Destiny Serna <destinyeserna@gmail.com>
2025-01-21 22:04:49 -0800

    added game board feature

--- seeker.py
+++ seeker.py
@@ -1,81 +1,77 @@
+import random
+
 def create_board(board_type):
-    board = [[]]
+
+    board = []
+
     if board_type == "game":
-        char = "X"
+        empty_space_char = "X"
     elif board_type == "user":
-        char = " "
+        empty_space_char = " "
     else:
-        char = "_"
-    for i in range(0,7):
-        row = []
-        for j in range(0,7):
-            if i == 0 and j == 0:
-                row.append(" - ")
-            elif i == 0:
+        empty_space_char = "_"
+
+    for row_num in range(0,7):
+        generated_row = []
+        for col_num in range(0,7):
+            if row_num == 0 and col_num == 0:
+                generated_row.append(" - ")
+            elif row_num == 0:
                 
-                row.append(" "+str(j)+" ")
-            elif j==0:
-                row.append(" "+str(i)+" ")
+                generated_row.append(" "+str(col_num)+" ")
+            elif col_num==0:
+                generated_row.append(" "+str(row_num)+" ")
             else:
-                row.append("["+char+"]")
-        board.append(row)
+                generated_row.append("["+empty_space_char+"]")
+        board.append(generated_row)
+
+    if board_type == "game":
+        l_location_row = random.randint(1,6)
+        l_location_col = random.randint(1,6)
+        board[l_location_row][l_location_col] = " L "
+        print("L is at row "+str(l_location_row)+" and col "+str(l_location_col))
+
     return board
+
+def print_board(board):
+    for row in board:
+        formatted_row = ""
+        for col in row:
+            formatted_row += col
+        print(formatted_row)
 
 class Game:
     playing = False
-    user_board = [[]]
     def __init__(self, score=0):
         self.score = score
         self.game_board = []
+        self.user_board = []
     
     def start_game(self):
         self.playing = True
-
         self.game_board = create_board("game")
         self.user_board = create_board("user")
-
-        # self.game_board = [["-",1,2,3,4,5],
-        #                    [1,"[X]","[X]","[X]","[X]","[X]"],
-        #                    [2,"[X]","[X]","[X]","[X]","[X]"],
-        #                    [3,"X","X","X","X","X"],
-        #                    [4,"X","X","X","X","X"],
-        #                    [5,"X","X","X","X","X"]]
-        # self.user_board = [["-",1,2,3,4,5],
-        #                    [1," "," "," "," "," "],
-        #                    [2," "," "," "," "," "],
-        #                    [3," "," "," "," "," "],
-        #                    [4," "," "," "," "," "],
-        #                    [5," "," "," "," "," "]]
-   
-    def format_board_square(self, char):
-        if char == "-":
-            return " - "
-        elif isinstance(char, int):
-            return " "+str(char)+" "
-        else:
-            return "["+char+"]"
-
-    def print_board(self,board):
-        for row in board:
-            formatted_row = ""
-            for col in row:
-                formatted_row += col
-            print(formatted_row)
+    
+    def print_game_board(self):
+        print_board(self.game_board)
+    
+    def print_user_board(self):
+        print_board(self.user_board)
 
 
 
 game = Game()
 game.start_game()
-game.print_board(game.game_board)
+game.print_game_board()
 
 print("")
 print("LETS PLAY SEEKER! :D")
 print("")
-start = input("PRESS ANY KEY TO START")
+start = input("PRESS ENTER TO START")
 print("")
 print("HERE IS THE BOARD")
 print("")
-game.print_board(game.user_board)
+game.print_user_board()
 print("")
 selected_row = input("SELECT A ROW: ")
 print("")
