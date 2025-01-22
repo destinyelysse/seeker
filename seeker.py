554b0fc00797b17cec0e822d128e048c2a1e0d64
Destiny Serna <destinyeserna@gmail.com>
2025-01-21 22:39:49 -0800

    Added gameplay mechanics: input selected space; update player board; determine W/L

--- seeker.py
+++ seeker.py
@@ -29,7 +29,11 @@ def create_board(board_type):
         l_location_row = random.randint(1,6)
         l_location_col = random.randint(1,6)
         board[l_location_row][l_location_col] = " L "
-        print("L is at row "+str(l_location_row)+" and col "+str(l_location_col))
+
+        w_location_row = random.randint(1,6)
+        w_location_col = random.randint(1,6)
+        board[l_location_row][l_location_col] = " W "
+
 
     return board
 
@@ -46,36 +50,57 @@ class Game:
         self.score = score
         self.game_board = []
         self.user_board = []
+        self.game_status = "not started"
+
+    def display_game_home(self):
+        print("")
+        print("LETS PLAY SEEKER! :D")
+        print("")
+        start = input("PRESS ENTER TO START")
+        print("")
+        self.start_game()
+
     
     def start_game(self):
-        self.playing = True
+        self.game_status = "playing"
         self.game_board = create_board("game")
         self.user_board = create_board("user")
+        self.get_player_turn_input()
     
     def print_game_board(self):
         print_board(self.game_board)
     
     def print_user_board(self):
         print_board(self.user_board)
+    
+    def get_player_turn_input(self):
+        print("YOUR BOARD:")
+        print("")
+        game.print_user_board()
+        print("")
+        print("SELECT A SPACE TO PLAY. ENTER ROW AND COLUMN NUMBER.")
+        print("")
+        selected_row = input("SELECT A ROW: ")
+        selected_col = input("SELECT A COLUMN: ")
+        print("")
+        print("YOU SELECTED ROW "+selected_row+" AND COLUMN "+selected_col)
+        print("")
+        self.play_space(int(selected_row),int(selected_col))
 
+    def play_space(self, row, col):
+        self.user_board[row][col] = self.game_board[row][col]
+        if self.game_board[row][col] == " L ":
+            print("OH NO - YOU GOT THE L! GAME OVER!")
+            self.print_user_board()
+        elif self.game_board[row][col] == " W ":
+            print("YAY! YOU GOT THE W. YOU WIN!")
+        else:
+            print("NICE! YOU ARE SAFE...FOR THIS ROUND.")
+            print("")
+            self.get_player_turn_input()
 
 
 game = Game()
-game.start_game()
-game.print_game_board()
+game.display_game_home()
 
-print("")
-print("LETS PLAY SEEKER! :D")
-print("")
-start = input("PRESS ENTER TO START")
-print("")
-print("HERE IS THE BOARD")
-print("")
-game.print_user_board()
-print("")
-selected_row = input("SELECT A ROW: ")
-print("")
-selected_column = input("SELECT A COLUMN: ")
-print("")
-print("YOU SELECTED ROW "+selected_row+" AND COLUMN "+selected_column)
-print("")
+
